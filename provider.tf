7e7780a7b3fee6315d36b543b778e8f44ec1c851
abakirov93 <63379267+abakirov93@users.noreply.github.com>
2020-12-01 16:50:56 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "${var.region}"
-  access_key = "AKIA6AGASNFSBDSLXL5F" # "AKIA6AGASNFSEMSRDXEY"
-  secret_key = "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" #
+  access_key = ""
+  secret_key = ""
 
 }
